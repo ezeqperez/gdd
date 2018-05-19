ea5691f6e6dc188da79b93429eab9cc41646b101
Ezequiel Perez <ezequiel.perez@mercadolibre.com>
2018-05-19 17:34:56 -0300

    Se modifica el login

--- src/FrbaHotel/Form1.Designer.cs
+++ src/FrbaHotel/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.confirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(36, 62);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(86, 25);
+            this.userLabel.TabIndex = 0;
+            this.userLabel.Text = "Usuario";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(36, 126);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(123, 25);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "Contraseña";
+            // 
+            // textBox1
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(184, 62);
+            this.userTextBox.Name = "textBox1";
+            this.userTextBox.Size = new System.Drawing.Size(250, 31);
+            this.userTextBox.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(184, 123);
+            this.passwordTextBox.Name = "textBox2";
+            this.passwordTextBox.Size = new System.Drawing.Size(250, 31);
+            this.passwordTextBox.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.confirm.Location = new System.Drawing.Point(184, 194);
+            this.confirm.Name = "button1";
+            this.confirm.Size = new System.Drawing.Size(131, 41);
+            this.confirm.TabIndex = 4;
+            this.confirm.Text = "Ingresar";
+            this.confirm.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 303);
+            this.Controls.Add(this.confirm);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.userLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button confirm;
     }
 }
 
